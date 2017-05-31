5b25792f429e644e76eb9ab16782e3000d8bf067
juliano.lopes <juliano.lopes@DSK15.FujidoBrasil.local>
2017-05-31 17:39:28 -0300

    Added QuickPasswordEnconding class

---
+++ src/br/com/fuji/mvc/util/QuickPasswordEncodingGenerator.java
@@ -0,0 +1,14 @@
+package br.com.fuji.mvc.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class QuickPasswordEncodingGenerator {
+
+	public static void main(String[] args) {
+
+		String password = "abc125";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		System.out.println(passwordEncoder.encode(password));
+	}
+
+}
